3ffe93fdcf8e6f4f106d3336e06a0141b167bd05
Kanya Zembe <zembekanya@gmail.com>
2023-09-25 18:22:54 +0200

    Card styling

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import { StyledCard } from './Styled/Card.Styled'
+import { StyledCard, SignUp, ImageContainer, Header, Body, Input, Image, List } from './Styled/Card.Styled'
 
 const Card = () => {
   return (
     <>
     <StyledCard>
-
-        
+      <SignUp>
+        <Header>Stay Updated!</Header>
+        <Body>Join 60 000+ product managers receiving monthly updates on:
+        <List>
+          <img src='./images/icon-list.svg' alt='icon' />
+          <p>Product discovery and building what matters</p>
+        </List>
+        <List>
+          <img src='./images/icon-list.svg' alt='icon' />
+          <p>Measuring to ensure updates are a success</p>
+        </List>
+        <List>
+          <img src='./images/icon-list.svg' alt='icon' />
+          <p>And much more!</p>
+        </List>
+        </Body>
+        <Input></Input>
+      </SignUp>
+      <ImageContainer>
+        <Image src='./images/illustration-sign-up-desktop.svg' alt='content psoter' />
+      </ImageContainer>
     </StyledCard>
     </>
   )
--- src/Components/Styled/Card.Styled.js
+++ src/Components/Styled/Card.Styled.js
@@ -1,10 +1,56 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.div`
-width: 700px;
-height: 480px;
+width: 800px;
+height: 500px;
 max-width: 100%;
 display: flex;
 background-color: hsl(0, 0%, 100%);
 border-radius: 25px;
+`
+export const SignUp = styled.div`
+max-width: 100%;
+width: 55%;
+height: 100%;
+display: flex;
+flex-direction: column;
+border-top-left-radius: 25px;
+border-bottom-left-radius: 25px;
+`
+export const ImageContainer = styled.div`
+width: 45%;
+height: 100%;
+background-color: coral;
+border-top-right-radius: 25px;
+border-bottom-right-radius: 25px;
+`
+export const Image = styled.div`
+display: flex;
+`
+export const Header = styled.h1`
+width: 100%;
+height: 20%;
+font-size: 350%;
+color: hsl(234, 29%, 20%);
+display: flex;
+align-items: center;
+padding: 0 8%;
+margin-top: 15%;
+`
+export const Body = styled.div`
+max-width: 100%;
+height: 50%;
+color: hsl(235, 18%, 26%);
+display: flex;
+flex-direction: column;
+`
+export const List = styled.p`
+width: 90%;
+height: 16.666%;
+display: flex;
+flex-direction: row;
+`
+export const Input = styled.div`
+width: 100%;
+height: 40%;
 `
