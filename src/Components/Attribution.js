1f0854f80691cc58c9d450e4d7eeabe6f801c91f
Kanya Zembe <zembekanya@gmail.com>
2023-09-27 17:12:40 +0200

    updated attribute

--- src/Components/Attribution.js
+++ src/Components/Attribution.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Attribution = () => {
   return (
-    <div>Attribution</div>
+    <div>
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="">Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/zembezn?tab=repositories">Kanya Zembe</a>.
+    </div>
   )
 }
 
