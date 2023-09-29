4c5d0680e6f4a8633d845e0733eed134b9b3f39d
Kanya Zembe <zembekanya@gmail.com>
2023-09-29 11:55:48 +0200

    Added attribution

--- src/Components/Attribution.js
+++ src/Components/Attribution.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Attribute } from './Styled/Card.Styled'
 
 const Attribution = () => {
   return (
-    <div>
+
+    <Attribute>
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="">Frontend Mentor</a>. 
       Coded by <a href="https://github.com/zembezn?tab=repositories">Kanya Zembe</a>.
-    </div>
+    </Attribute>
+    
   )
 }
 
