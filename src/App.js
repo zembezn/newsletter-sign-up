4035c33f97bd0e7f442386fb453e4833761e7924
Kanya Zembe <zembekanya@gmail.com>
2023-09-29 11:55:48 +0200

    added attribution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyles from "./Components/Styled/GlobalStyles";
 import Card from "./Components/Card";
-
+import Attribution from "./Components/Attribution"
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <Card />
+      <Attribution />
     </>
   );
 }
